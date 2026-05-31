083476d229d6696ca32f9dcc2938f44fa5345566
Slizzle45 <efecorukoglu@gmail.com>
2026-05-31 21:37:39 +0300

    Change HOST address to allow external access

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 STORAGE_ROOT = (Path(__file__).resolve().parent.parent / "storage").resolve()
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 PORT = 8000
 
 STORAGE_ROOT.mkdir(parents=True, exist_ok=True)
